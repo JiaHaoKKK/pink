29b940136f97f9cb6a598b556932ded7463b234d
qiyi <bphanzhu@gmail.com>
2017-12-10 03:18:04 +0800

    修正集成测试时解密失败的问题

--- src/test/java/com/momo/pink/it/UserTest.java
+++ src/test/java/com/momo/pink/it/UserTest.java
@@ -7,16 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = PinkApp.class, properties = {
+    "encrypt.key=${encrypt.rootKey}oUzxewPh",
     "spring.test.maria.database=pink",
     "spring.datasource.username=root",
     "spring.datasource.password=",
